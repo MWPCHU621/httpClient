2882c3c13e0dce10dc57cce2af198ac128a274da
Patrick Chu <mingweipatrickc@gmail.com>
2018-09-03 15:08:46 +0000

    created function to get from option and print using buffering technique

--- getPrintHtml.js
+++ getPrintHtml.js
@@ -1,11 +1,10 @@
 var https = require('https');
 
-
 //gets HTML chunks and console.log each chunk of data as it is received,
 //mconcatenated with a newline character ('\n') so you can visualize each chunk.
-function getAndPrintHTML () {
+function getAndPrintHTML() {
 
-  var fileData = [];
+  var fileData = "";
 
   var requestOptions = {
     host: 'sytantris.github.io',
@@ -15,15 +14,15 @@ function getAndPrintHTML () {
   //gets chunk from host and given file path.
   https.get(requestOptions, function(response)
   {
+
     //sets incoming response as utf8.
     response.setEncoding('utf8');
 
 
     response.on('data', function (data) {
-
-
-      // console.log('Chunk Received. Data is:', data + "\n");
-
+      fileData += (data + "\n");
+      //console.log(fileData);
+      //console.log('Chunk Received. Data is:', data + "chunk count is: " + count);
     });
 
     response.on('end', function() {
