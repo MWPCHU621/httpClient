181bcb397058fa5e3770fbe1b2fcc77872ac7c96
Patrick Chu <mingweipatrickc@gmail.com>
2018-09-03 16:08:50 +0000

    created file that includes a function that  prints using callback function instead of console.log

---
+++ printHtml.js
@@ -0,0 +1,44 @@
+var https = require('https');
+
+var fileData = "";
+
+function getHTML (options, callback) {
+
+
+
+  //gets chunk from host and given file path.
+  https.get(options, function(response)
+  {
+
+    //sets incoming response as utf8.
+    response.setEncoding('utf8');
+
+
+    response.on('data', function (data) {
+      fileData += (data + "\n");
+      //console.log(fileData);
+      //console.log('Chunk Received. Data is:', data + "chunk count is: " + count);
+    });
+
+    response.on('end', function() {
+      console.log('Response stream complete.');
+      callback(fileData);
+
+    });
+
+
+
+  });
+
+}
+
+function printHTML (html) {
+  console.log(html);
+}
+
+var requestOptions = {
+  host: 'sytantris.github.io',
+  path: '/http-examples/step4.html'
+};
+
+getHTML(requestOptions, printHTML);
